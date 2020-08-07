764e9055eeb85799cf1532b90e2a1af64b3eb5dc
voncleyton <martins.voncleyton@gmail.com>
2020-08-06 20:25:41 -0400

    add features

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const productExits = await this.productsRepository.findByName(name);
+
+    if (productExits) {
+      throw new AppError('This product already exists.');
+    }
+
+    const product = this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
